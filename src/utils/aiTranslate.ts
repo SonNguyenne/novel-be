4432a544c4f3712c43164291ab3d6e56d63c36d2
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2024-02-29 09:49:04 +0700

    Change AI tool from GPT to Gemini

--- src/utils/aiTranslate.ts
+++ src/utils/aiTranslate.ts
@@ -1,32 +1,13 @@
-import OpenAI from 'openai';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
 export const aiTranslate = async (body: string, type?: string) => {
-  const content: string = `${
-    type !== 'content' ? 'Trả lời ngắn gọn cho' : ''
-  } Kết quả của dịch ${
-    type == 'author' ? `tên tác giả` : `đoạn văn bản`
-  } dưới đây sang tiếng Việt một cách tự nhiên nhất: ${body}.`;
+  const prompt = `Dịch đoạn tiếng Trung này từ truyện sang tiếng Việt: ${body}`;
 
-  const data = await openai.chat.completions.create({
-    messages: [
-      {
-        role: 'user',
-        content,
-      },
-    ],
-    model: 'gpt-3.5-turbo',
-  });
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
 
-  const result: string = data['choices'][0]['message']['content'];
-  var matches = result.match(/"([^"]+)"/g);
-
-  if (matches) {
-    return matches[1].replace(/"/g, '');
-  } else {
-    return result;
-  }
+  return response.text();
 };
