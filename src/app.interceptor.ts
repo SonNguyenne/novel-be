aedb379b021cd99656f3ff66f6b7c7b7f96f4b46
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2024-10-13 10:37:57 +0700

    Modify logging

--- src/app.interceptor.ts
+++ src/app.interceptor.ts
@@ -11,24 +11,30 @@ export class LoggingInterceptor implements NestInterceptor {
     const res = context.switchToHttp().getResponse()
     const now = Date.now()
 
-    const logParts = [
-      `[${req.method}] - ${req.url}`,
-      req.query && Object.keys(req.query).length ? `QUERY - ${JSON.stringify(req.query)}` : '',
-      req.params && Object.keys(req.params).length ? `PARAMS - ${JSON.stringify(req.params)}` : '',
-      req.body && Object.keys(req.body).length ? `BODY - ${JSON.stringify(req.body)}` : '',
-      `\x1b[36m${req.ip}\x1b[36m`,
-      `${this._color(res.statusCode)} \x1b[33m+${Date.now() - now} ms\x1b[33m`,
-    ]
-
-    const logMessage = logParts.filter(part => part).join(' | ')
-
     return next.handle().pipe(
-      tap(() => req.url && this.logger.log(logMessage)),
-      catchError(error => {
-        req.url && this.logger.error(logMessage)
-        throw error
+      tap(() => {
+        const logMessage = this.buildLogMessage(req, res, now)
+        req.url && this.logger.log(logMessage)
+      }),
+      catchError(err => {
+        const logMessage = this.buildLogMessage(req, res, now, err.status)
+        req.url && this.logger.error(`${logMessage} | \x1b[31m[Error] - ${err.message}\x1b[31m`)
+        throw err
       }),
     )
+  }
+
+  private buildLogMessage(req: any, res: any, now: number, statusCode?: number): string {
+    const logParts = [
+      `[${req.method}] - ${req.url}`,
+      req.query && Object.keys(req.query).length ? `[Query] - ${JSON.stringify(req.query)}` : '',
+      req.params && Object.keys(req.params).length ? `[Params] - ${JSON.stringify(req.params)}` : '',
+      req.body && Object.keys(req.body).length ? `[Body] - ${JSON.stringify(req.body)}` : '',
+      `\x1b[36m${req.ip}\x1b[36m`,
+      `${this._color(statusCode ?? res.statusCode)} \x1b[33m+${Date.now() - now} ms\x1b[33m`,
+    ]
+
+    return logParts.filter(part => part).join(' | ')
   }
 
   private _color(statusCode: number): string {
@@ -39,9 +45,9 @@ export class LoggingInterceptor implements NestInterceptor {
 
     if (statusCode >= 200 && statusCode < 300) {
       return `${green}${statusCode}${reset}`
-    } else if (statusCode >= 400 && statusCode < 500) {
+    } else if (statusCode >= 300 && statusCode < 400) {
       return `${yellow}${statusCode}${reset}`
-    } else if (statusCode >= 500) {
+    } else if (statusCode >= 400) {
       return `${red}${statusCode}${reset}`
     }
 
